337925c5a59b0d85e2a47556f0f64cef1bb03d83
Eduardo Diez <edudm95@gmail.com>
2022-11-09 11:32:23 +0100

    Incluido fondo en el login

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,37 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        web: {
+          boton: "#56B6CB",
+          fondo: "#C9D5E0",
+          topBar: "#4FA9A9",
+          loginButton: "#A5E1AD",
+        },
+      },
+
+      boxShadow: {
+        input: "inset 5px 5px 10px #AAAAAA",
+        login: "5px 5px 10px #5555555"
+      },
+
+      fontFamily: {
+        bluetea: ["bluetea", "cursive"],
+      },
+
+      height: {
+        login: "600px"
+      },
+
+      width: {
+        login: "450px"
+      },
+
+      borderRadius: {
+        login: "40px"
+      }
+    },
   },
   plugins: [],
 };
